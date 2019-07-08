ada1426672d793fc973ec152834f5ec979f2bed5
Emory <Emory@Remine.com>
2019-07-07 21:42:41 -0400

    sign

--- scripts/git-prepare/git-prepare.sh
+++ scripts/git-prepare/git-prepare.sh
@@ -6,7 +6,7 @@ git commit --all --allow-empty --message "preparation commit" &&
         git checkout -b scratch_$(uuidgen) &&
             COMMENT_COMMIT=$(git log --pretty=format:"%h" "upstream/${UPSTREAM_BRANCH}"..HEAD | tail -n 1) &&
             git reset --soft "upstream/${UPSTREAM_BRANCH}" &&
-            git commit --reedit-message ${COMMENT_COMMIT}
+            git commit -S --reedit-message ${COMMENT_COMMIT}
     else
         echo Failed to cleanly refresh &&
             exit 64
