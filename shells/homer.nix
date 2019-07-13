4843f1d4704f380eb987f1b8a216ca7fd7ab2a7b
Emory <Emory@Remine.com>
2019-07-13 01:07:20 +0000

    homey

--- shells/homer.nix
+++ shells/homer.nix
@@ -1,5 +1,19 @@
 {
-  staples
+  staples,
+  hook ? { staples } : ''
+    HOME=$(${staples.pkgs.mktemp}/bin/mktemp -d) &&
+      cleanup(){
+        ${staples.pkgs.cowsay}/bin/cowsay Good Bye &&
+          ${staples.pkgs.coreutils}/bin/rm --recursive --force $HOME &&
+	  true
+      } &&
+      trap cleanup EXIT &&
+      ${staples.pkgs.cowsay}/bin/cowsay Hello &&
+      true
+  '',
+  input ? { staples } : [
+    staples.pkgs.atari800
+  ]
 } :
 staples.pkgs.mkShell {
   shellHook = ''
